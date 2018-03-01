71db50d75f0efcf24141501418c857396a9171e5
Halimazeez <Halim.azeez@googlemail.com>
2018-03-01 23:38:59 +0000

    changed browserroute to hash route for github pages

--- src/index.js
+++ src/index.js
@@ -1,14 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import registerServiceWorker from "./registerServiceWorker";
 
 ReactDOM.render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>,
-  document.getElementById("root")
+	<HashRouter>
+		<App />
+	</HashRouter>,
+	document.getElementById("root")
 );
 
 registerServiceWorker();
